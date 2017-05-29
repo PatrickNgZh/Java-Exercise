5d896db64bd85cc3e1d292d0f5be898cd1a645a4
PatrickNg <PatrickNgZh@outlook.com>
2017-05-29 20:29:04 +0800

    fix

---
+++ TestInput/src/Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Test {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner cin=new Scanner(System.in);
+		while(cin.hasNextInt()){
+			a=cin.nextInt();
+			b=cin.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
